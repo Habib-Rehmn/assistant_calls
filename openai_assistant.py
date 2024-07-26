6f96dcaf30a6798f2e3e159405ce2f8515200158
Habib-Rehmn <habibeternal@gmail.com>
2024-07-26 17:39:03 +0500

    added file search tool

--- openai_assistant.py
+++ openai_assistant.py
@@ -7,9 +7,11 @@ open_ai_key = os.getenv("OPENAI_API_KEY")
  
 # Define Functions
 assistant = client.beta.assistants.create(
-  instructions="You are a weather bot. Use the provided functions to answer questions.",
+  name="Coach V",
+  instructions="You are a weather bot. Use the provided functions to answer questions. and you will also give answer to the user questoin based on the retrieved context about shot pulse sp and the setup instructions for shot pulse sp",
   model="gpt-4o-mini-2024-07-18",
   tools=[
+    {"type": "file_search"},
     {
       "type": "function",
       "function": {
@@ -53,13 +55,40 @@ assistant = client.beta.assistants.create(
 )
 
 
+# Create a vector store caled "SP"
+vector_store = client.beta.vector_stores.create(name="tool_test")
+
+# Ready the files for upload to OpenAI
+file_path = 'sp_data.txt'
+file_streams = [open(file_path, "rb")]
+
+
+# Use the upload and poll SDK helper to upload the files, add them to the vector store,
+# and poll the status of the file batch for completion.
+file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+  vector_store_id=vector_store.id, files=file_streams
+)
+
+# You can print the status and the file counts of the batch to see the result of this operation.
+print(file_batch.status)
+print(file_batch.file_counts)
+print(vector_store.id)
+
+
+#Update the assistant to to use the new Vector Store
+assistant = client.beta.assistants.update(
+  assistant_id=assistant.id,
+  tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+)
+
+
 
 #Create a Thread and Add a message
 thread = client.beta.threads.create()
 message = client.beta.threads.messages.create(
   thread_id=thread.id,
   role="user",
-  content="What's the weather in San Francisco today and the likelihood it'll rain?",
+  content="What is sp",
 )
 
 
